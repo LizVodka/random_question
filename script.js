cc538a5564addc27cbe9fba37460fd71990acce4
LizVodka <emmacska2330@gmail.com>
2022-02-01 20:09:52 +0100

    first try

---
+++ script.js
@@ -0,0 +1,32 @@
+$(function() {
+    let questions = [];
+    let n;
+    let asked = [];
+
+    $.get('questions.txt', function(data) {
+        questions = data.split("\n");
+        n = questions.length;
+    });
+
+    $(document).ready(function() {
+        new_question(questions);
+    })
+
+    function new_question(questions) {
+        do {
+            x = Math.floor((Math.random() * n) + 1);
+            valid = jQuery.inArray(x, asked) == -1;
+        } while (valid == false);
+        question = questions[x];
+        $("#question").html(question);
+        add_to_asked(asked, x);
+    }
+    
+    function add_to_asked(asked, x) {
+        asked.push(x);
+    }
+
+    $('button').click(function(){
+        new_question(questions);
+    })
+})
